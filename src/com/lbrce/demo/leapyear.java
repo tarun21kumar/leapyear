689ecbbeb2df8cfb34f7b8a6249ae92a14c9977a
Tarun Kumar Bezawada <Tarun Kumar Bezawada@LAPTOP-FFJ34EKP>
2023-08-16 22:24:19 +0530

    2020 is leap year

---
+++ src/com/lbrce/demo/leapyear.java
@@ -0,0 +1,19 @@
+package com.lbrce.demo;
+
+public class leapyear {
+	public class Main {
+		  public static void main(String[] args) {
+		    int year = 2020;
+		    boolean isLeapYear;
+		    isLeapYear = (year % 4 == 0);
+		    isLeapYear = isLeapYear && (year % 100 != 0 || year % 400 == 0);
+		    if (isLeapYear) {
+		      System.out.println(year + " is a leap year.");
+		    } else {
+		      System.out.println(year + " is not a leap year.");
+		    }
+		  }
+		}
+
+
+}
